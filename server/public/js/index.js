45e5e3fae3056f9bd34761a1df87f43bfa418ea5
Gary ge <ge.sf.chn@gmail.com>
2017-09-06 22:58:37 +0800

    4th

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -3988,7 +3988,11 @@ module.exports = __webpack_require__(125);
 
 __webpack_require__(126);
 
-__webpack_require__(329);
+var _lottery = __webpack_require__(329);
+
+var _lottery2 = _interopRequireDefault(_lottery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /***/ }),
 /* 126 */
@@ -9587,230 +9591,92 @@ module.exports = function (regExp, replace) {
 "use strict";
 
 
-{
-  // genertaor基本定义
-  var tell = /*#__PURE__*/regeneratorRuntime.mark(function tell() {
-    return regeneratorRuntime.wrap(function tell$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return 'a';
+__webpack_require__(330);
 
-          case 2:
-            _context.next = 4;
-            return 'b';
+__webpack_require__(331);
 
-          case 4:
-            return _context.abrupt('return', 'c');
+__webpack_require__(332);
 
-          case 5:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, tell, this);
-  });
+__webpack_require__(333);
 
-  var k = tell();
+/***/ }),
+/* 330 */
+/***/ (function(module, exports, __webpack_require__) {
 
-  console.log(k.next());
-  console.log(k.next());
-  console.log(k.next());
-  console.log(k.next());
-}
+"use strict";
 
-{
-  var obj = {};
-  obj[Symbol.iterator] = /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    return regeneratorRuntime.wrap(function _callee$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            _context2.next = 2;
-            return 1;
 
-          case 2:
-            _context2.next = 4;
-            return 2;
+/***/ }),
+/* 331 */
+/***/ (function(module, exports, __webpack_require__) {
 
-          case 4:
-            _context2.next = 6;
-            return 3;
+"use strict";
 
-          case 6:
-          case 'end':
-            return _context2.stop();
-        }
-      }
-    }, _callee, this);
-  });
 
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  try {
-    for (var _iterator = obj[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var value = _step.value;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-      console.log('value', value);
+var Timer = function () {
+    function Timer() {
+        _classCallCheck(this, Timer);
     }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
-}
 
-{
-  var state = /*#__PURE__*/regeneratorRuntime.mark(function state() {
-    return regeneratorRuntime.wrap(function state$(_context3) {
-      while (1) {
-        switch (_context3.prev = _context3.next) {
-          case 0:
-            if (false) {
-              _context3.next = 9;
-              break;
+    _createClass(Timer, [{
+        key: 'countdown',
+        value: function countdown(end, update, handle) {
+            var now = new Date().getTime();
+            var self = this;
+            if (now - end) {
+                handle.call(self);
+            } else {
+                var last_time = end - now;
+                var px_d = 1000 * 60 * 60 * 24;
+                var px_h = 1000 * 60 * 60;
+                var px_m = 1000 * 60;
+                var px_s = 1000;
+                var d = Math.floor(last_time / px_d);
+                var h = Math.floor((last_time - d * px_d) / px_d);
+                var m = Math.floor((last_time - d * px_d - h * px_h) / px_m);
+                var s = Math.floor((last_time - d * px_d - h * px_h - m * px_m) / px_s);
+                var r = [];
+                if (d > 0) {
+                    r.push('<em>' + d + '</em>\u5929');
+                }
+                if (r.lenth || h > 0) {
+                    r.push('<em>' + d + '</em>\u65F6');
+                }
+                if (r.length || m > 0) {
+                    r.push('<em>' + m + '</em>\u5206');
+                }
+                if (r.length || s > 0) {
+                    r.push('<em>' + s + '</em>\u79D2');
+                }
+                self.last_time = r.join('');
+                update.call(self, r.join(''));
+                setTimeout(function () {
+                    self.countdown(end, update, handle);
+                }, 1000);
             }
-
-            _context3.next = 3;
-            return 'A';
-
-          case 3:
-            _context3.next = 5;
-            return 'B';
-
-          case 5:
-            _context3.next = 7;
-            return 'C';
-
-          case 7:
-            _context3.next = 0;
-            break;
-
-          case 9:
-          case 'end':
-            return _context3.stop();
         }
-      }
-    }, state, this);
-  });
-  var status = state();
-  console.log(status.next());
-  console.log(status.next());
-  console.log(status.next());
-  console.log(status.next());
-  console.log(status.next());
-}
+    }]);
 
-// {
-//   let state=async function (){
-//     while(1){
-//       await 'A';
-//       await 'B';
-//       await 'C';
-//     }
-//   }
-//   let status=state();
-//   console.log(status.next());
-//   console.log(status.next());
-//   console.log(status.next());
-//   console.log(status.next());
-//   console.log(status.next());
-// }
+    return Timer;
+}();
 
-{
-  var draw = function draw(count) {
-    //具体抽奖逻辑
-    console.info('\u5269\u4F59' + count + '\u6B21');
-  };
+/***/ }),
+/* 332 */
+/***/ (function(module, exports, __webpack_require__) {
 
-  var residue = /*#__PURE__*/regeneratorRuntime.mark(function residue(count) {
-    return regeneratorRuntime.wrap(function residue$(_context4) {
-      while (1) {
-        switch (_context4.prev = _context4.next) {
-          case 0:
-            if (!(count > 0)) {
-              _context4.next = 6;
-              break;
-            }
+"use strict";
 
-            count--;
-            _context4.next = 4;
-            return draw(count);
 
-          case 4:
-            _context4.next = 0;
-            break;
+/***/ }),
+/* 333 */
+/***/ (function(module, exports, __webpack_require__) {
 
-          case 6:
-          case 'end':
-            return _context4.stop();
-        }
-      }
-    }, residue, this);
-  });
+"use strict";
 
-  var star = residue(5);
-  var btn = document.createElement('button');
-  btn.id = 'start';
-  btn.textContent = '抽奖';
-  document.body.appendChild(btn);
-  document.getElementById('start').addEventListener('click', function () {
-    star.next();
-  }, false);
-}
-
-{
-  //长轮询,实时访问服务器
-  //websocket浏览器支持不好
-  var ajax = /*#__PURE__*/regeneratorRuntime.mark(function ajax() {
-    return regeneratorRuntime.wrap(function ajax$(_context5) {
-      while (1) {
-        switch (_context5.prev = _context5.next) {
-          case 0:
-            _context5.next = 2;
-            return new Promise(function (resolve, reject) {
-              setTimeout(function () {
-                resolve({ code: 0 });
-              }, 200);
-            });
-
-          case 2:
-          case 'end':
-            return _context5.stop();
-        }
-      }
-    }, ajax, this);
-  });
-
-  var pull = function pull() {
-    var genertaor = ajax();
-    var step = genertaor.next();
-    step.value.then(function (d) {
-      if (d.code != 0) {
-        setTimeout(function () {
-          console.info('wait');
-          pull();
-        }, 1000);
-      } else {
-        console.info(d);
-      }
-    });
-  };
-
-  pull();
-}
 
 /***/ })
 /******/ ]);
